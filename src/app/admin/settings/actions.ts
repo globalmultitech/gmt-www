6f29d6b43a9bd3e01736711912c6bac650d1ebdd
Ekky Pradipta <eqpradipta@gmail.com>
2025-07-10 18:12:38 +0000

    masih sarua errorna mang

--- src/app/admin/settings/actions.ts
+++ src/app/admin/settings/actions.ts
@@ -92,7 +92,7 @@ const SettingsSchema = z.object({
       return z.NEVER;
     }
   }),
-  // Blog Posts
+  // Blog Posts - THIS WAS MISSING
   blogPosts: z.string().transform((str, ctx) => {
     try {
       return JSON.parse(str);
