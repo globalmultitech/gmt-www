6f9c3bf60651e5a936b374f764ac73e5798ae91e
Ekky Pradipta <eqpradipta@gmail.com>
2025-07-23 17:24:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <GlobalSearch>
    
    wi

--- src/components/global-search.tsx
+++ src/components/global-search.tsx
@@ -44,17 +44,12 @@ export default function GlobalSearch({ products }: GlobalSearchProps) {
     <>
       <Button
         variant="outline"
-        className={cn(
-          'relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2'
-        )}
+        size="icon"
+        className="h-9 w-9"
         onClick={() => setOpen(true)}
       >
-        <Search className="h-4 w-4 xl:mr-2" aria-hidden="true" />
-        <span className="hidden xl:inline-flex">Cari produk...</span>
+        <Search className="h-4 w-4" aria-hidden="true" />
         <span className="sr-only">Cari produk</span>
-        <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 xl:flex">
-          <span className="text-xs">⌘</span>K
-        </kbd>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder="Ketik nama produk..." />
